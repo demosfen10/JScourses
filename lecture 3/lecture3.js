35bf6dcd25cb599e7c309d6ae20c0b8024a6c031
demosfen10 <32422011+demosfen10@users.noreply.github.com>
2018-11-19 19:13:51 +0200

    Update lecture3.js

--- lecture 3/lecture3.js
+++ lecture 3/lecture3.js
@@ -1,36 +1,18 @@
-function Q1 (FirstNumber,SecondNubmer){
-    if (FirstNumber>SecondNubmer){
+function f2(b) //проверка на undefined
+{ return b==undefined } 
+
+function f3(a) //проверка на null
+{
+    return a==null;
+}
+ function f4(c) //проверка на NaN
+ {return c!==c;}
+ 
+ function isEmptyObject(obj) { //проверка, что объект пустой(не имеет свойств)
+    for (var i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            return false;
+        }
+    }
     return true;
-    }
-    else{
-    return false;
-    }
-    } 
-    
-    function Q2(SmthString){
-    console.log("Вы ввели:"+ SmthString);
-    }
-    
-    function Q3(Any){
-    if(Any==null || Any==undefined){
-    return true;
-    }
-    else{
-    return false;
-    }
-    }
-    
-    function Q4(SmthObject){
-    SmthObject = {
-    checked:true
-    }
-    }
-    
-    function Q5 (f) {
-    for (i = 0; i < f; i++){
-    console.log(i); 
-    }
-    for (i = f; i >= 0; i--){
-    console.log(i);
-    } 
-    }
+}
