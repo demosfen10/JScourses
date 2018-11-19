c049be6c1394c1993ab22ffd3c4fec37a0f00e23
demosfen10 <32422011+demosfen10@users.noreply.github.com>
2018-11-19 18:38:20 +0200

    Update ht.js

--- ht.js
+++ ht.js
@@ -1,5 +1,5 @@
-﻿function Q1 (FirstNumber,SecondNubmer){
-    if (FirstNumber>SecondNubmer){
+﻿function Q1 (FirstNumber,SecondNubmer){ //функция, которая принимает 2 параметра - 2 числа и возвращает true, 
+    if (FirstNumber>SecondNubmer){//если первое число больше второго, и false, если это не так
     return true;
     }
     else{
@@ -7,11 +7,11 @@
     }
     } 
     
-    function Q2(SmthString){
+    function Q2(SmthString){//функция, которая принимает 1 параметр - строку и возвращает новую строку вида, “Вы ввели * полученная строка *”.
     console.log("Вы ввели:"+ SmthString);
     }
     
-    function Q3(Any){
+    function Q3(Any){//функция, которая принимает 1 параметр любого типа и проверяет, является ли полученное значение null или undefined.
     if(Any==null || Any==undefined){
     return true;
     }
@@ -20,13 +20,14 @@
     }
     }
     
-    function Q4(SmthObject){
+    function Q4(SmthObject){//функция, которая принимает 1 параметр - объект и добавляет этому объекту поле checked со значением true.
+
     SmthObject = {
     checked:true
     }
     }
     
-    function Q5 (f) {
+    function Q5 (f) {//функцию, которая принимает 1 параметр - число и выводит в консоль все числа от нуля до введенного числа (в обе стороны).
     for (i = 0; i < f; i++){
     console.log(i); 
     }
