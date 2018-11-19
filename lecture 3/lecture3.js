64e2f53d416f42f9a2016af45b0a553ad9332b2a
demosfen10 <32422011+demosfen10@users.noreply.github.com>
2018-11-19 19:10:48 +0200

    Create lecture3.js

---
+++ lecture 3/lecture3.js
@@ -0,0 +1,36 @@
+function Q1 (FirstNumber,SecondNubmer){
+    if (FirstNumber>SecondNubmer){
+    return true;
+    }
+    else{
+    return false;
+    }
+    } 
+    
+    function Q2(SmthString){
+    console.log("Вы ввели:"+ SmthString);
+    }
+    
+    function Q3(Any){
+    if(Any==null || Any==undefined){
+    return true;
+    }
+    else{
+    return false;
+    }
+    }
+    
+    function Q4(SmthObject){
+    SmthObject = {
+    checked:true
+    }
+    }
+    
+    function Q5 (f) {
+    for (i = 0; i < f; i++){
+    console.log(i); 
+    }
+    for (i = f; i >= 0; i--){
+    console.log(i);
+    } 
+    }
